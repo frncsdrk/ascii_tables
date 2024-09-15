a8cd0b70efe7768425b7c815d14e7989307c3f2f
frncsdrk <git@frncsdrk.me>
2024-09-15 05:02:40 +0200

    [docs] Document todos

--- src/tables.cr
+++ src/tables.cr
@@ -1,6 +1,11 @@
-# TODO: Write documentation for `Tables`
+# Placeholder for Table namespace
 module Tables
-  VERSION = "0.1.0"
+  VERSION = {{ `shards version #{__DIR__}`.chomp.stringify }}
 
+  # TODO: Implement input type (Array(Array(String)))
+  # TODO: Implement configurable separators (input, output)
+  # TODO: Implement configurable headers
+  # TODO: Implement strict mode (check number of items per line)
+  # TODO: Implement markdown notation
   # TODO: Put your code here
 end
