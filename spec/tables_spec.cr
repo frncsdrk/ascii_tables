4b83bf45c88da788b75eaaa5dd6cc1295196b66f
frncsdrk <git@frncsdrk.me>
2024-09-16 23:59:23 +0200

    WIP

--- spec/tables_spec.cr
+++ spec/tables_spec.cr
@@ -3,7 +3,13 @@ require "./spec_helper"
 describe Tables do
   # TODO: Write tests
 
-  it "works" do
-    false.should eq(true)
+  it "tracks the version number" do
+    Tables::VERSION.blank?.should eq(false)
+  end
+
+  it "renders" do
+    config = Tables::TableConfig.new
+    config.headers = ["one", "two", "three"]
+    Tables.render([["helo", ",", "world"]], config)
   end
 end
--- src/tables.cr
+++ src/tables.cr
@@ -2,10 +2,53 @@
 module Tables
   VERSION = {{ `shards version #{__DIR__}`.chomp.stringify }}
 
-  # TODO: Implement input type (Array(Array(String)))
-  # TODO: Implement configurable separators (input, output)
+  # TODO: Implement config type (struct)
+  # TODO: Implement configurable separators (output)
   # TODO: Implement configurable headers
   # TODO: Implement strict mode (check number of items per line)
   # TODO: Implement markdown notation
   # TODO: Put your code here
+
+  def self.render(data : Array(Array(String)), config : TableConfig = TableConfig.new)
+    # prepare
+    prev_row_size = nil
+    cell_lengths = Array(Int32).new
+    config.headers.each do |header|
+      cell_lengths.push header.size
+    end
+    data.each do |row|
+      if config.strict && prev_row_size != nil && prev_row_size != row.size
+        # throw error in strict mode
+      end
+
+      new_row = row.sort_by { |item| item.size }
+      puts new_row
+      puts row
+
+      row.each_with_index do |v, i|
+        unless cell_lengths[i]?
+          cell_lengths[i] = v.size
+        end
+        if v.size > cell_lengths[i]
+          cell_lengths[i] = v.size
+        end
+      end
+    end
+    puts "cell lengths:", cell_lengths
+    # /prepare
+  end
+
+  struct TableConfig
+    property headers : Array(String)
+    property markdown : Bool
+    property separator : String
+    property strict : Bool
+
+    def initialize
+      @headers = Array(String).new
+      @markdown = false
+      @separator = "|"
+      @strict = true
+    end
+  end
 end
